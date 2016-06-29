d6d306e63dbef72c496c46766ce11af96a659055
Nemanja Tozic <nemanjat94@hotmail.com>
2016-06-30 00:10:21 +0200

    Minor changes to the filler factory

--- gw_service/src/main/java/rs/edu/viser/services/filler/Filler.java
+++ gw_service/src/main/java/rs/edu/viser/services/filler/Filler.java
@@ -3,31 +3,40 @@ package rs.edu.viser.services.filler;
 import rs.edu.viser.services.filler.config.FillerArrayPatternConfig;
 import rs.edu.viser.services.filler.config.FillerConfigReader;
 import rs.edu.viser.services.filler.config.FillerObjectPatternConfig;
+import rs.edu.viser.services.filler.groups.FillerGroup;
+import rs.edu.viser.services.filler.groups.FillerGroup.FillerGroupTypes;
 
 public class Filler {
 	
 	public static void main(String[] args) {
-		FillerConfigReader reader = FillerConfigReader.getFillerConfigReader();
-		System.out.println(reader.getWebsite());
-		for (int i = 0; i < reader.getFillerGroups().length; i++) {
-			System.out.println("\t" + reader.getFillerGroups()[i].getUrlSufix());
-			System.out.println("\t" + reader.getFillerGroups()[i].getFillerGroupType());
-			System.out.println("\t" + reader.getFillerGroups()[i].getSchedulerType());
-			for (int j = 0; j < reader.getFillerGroups()[i].getFillerPatterns().length; j++) {
-				System.out.println("\t\t" + reader.getFillerGroups()[i].getFillerPatterns()[j].getUrlPattern());
-				System.out.println("\t\tfull url: " + reader.getWebsite() + reader.getFillerGroups()[i].getUrlSufix() + reader.getFillerGroups()[i].getFillerPatterns()[j].getUrlPattern());
-				if (reader.getFillerGroups()[i].getFillerPatterns()[j] instanceof FillerObjectPatternConfig) {
-					FillerObjectPatternConfig obj = 
-							(FillerObjectPatternConfig)reader.getFillerGroups()[i].getFillerPatterns()[j];
-					System.out.println("\t\t" + obj.getObject());
-				} else if (reader.getFillerGroups()[i].getFillerPatterns()[j] instanceof FillerArrayPatternConfig) {
-					FillerArrayPatternConfig obj = 
-							(FillerArrayPatternConfig)reader.getFillerGroups()[i].getFillerPatterns()[j];
-					System.out.println("\t\t" + obj.getPropertyName());
-				}
-				System.out.println();
-			}
-			System.out.println();
+//		FillerConfigReader reader = FillerConfigReader.getFillerConfigReader();
+//		System.out.println(reader.getWebsite());
+//		for (int i = 0; i < reader.getFillerGroups().length; i++) {
+//			System.out.println("\t" + reader.getFillerGroups()[i].getUrlSufix());
+//			System.out.println("\t" + reader.getFillerGroups()[i].getFillerGroupType());
+//			System.out.println("\t" + reader.getFillerGroups()[i].getSchedulerType());
+//			for (int j = 0; j < reader.getFillerGroups()[i].getFillerPatterns().length; j++) {
+//				System.out.println("\t\t" + reader.getFillerGroups()[i].getFillerPatterns()[j].getUrlPattern());
+//				System.out.println("\t\tfull url: " + reader.getWebsite() + reader.getFillerGroups()[i].getUrlSufix() + reader.getFillerGroups()[i].getFillerPatterns()[j].getUrlPattern());
+//				if (reader.getFillerGroups()[i].getFillerPatterns()[j] instanceof FillerObjectPatternConfig) {
+//					FillerObjectPatternConfig obj = 
+//							(FillerObjectPatternConfig)reader.getFillerGroups()[i].getFillerPatterns()[j];
+//					System.out.println("\t\t" + obj.getObject());
+//				} else if (reader.getFillerGroups()[i].getFillerPatterns()[j] instanceof FillerArrayPatternConfig) {
+//					FillerArrayPatternConfig obj = 
+//							(FillerArrayPatternConfig)reader.getFillerGroups()[i].getFillerPatterns()[j];
+//					System.out.println("\t\t" + obj.getPropertyName());
+//				}
+//				System.out.println();
+//			}
+//			System.out.println();
+//		}
+//		
+//		System.out.println("-----");
+		
+		FillerFactory ff = new FillerFactory().fillerGroups(FillerGroupTypes.ACCOUNT);
+		for (FillerGroup fg : ff.getFillerGroup()) {
+			System.out.println(fg);
 		}
 	}
 
--- gw_service/src/main/java/rs/edu/viser/services/filler/FillerFactory.java
+++ gw_service/src/main/java/rs/edu/viser/services/filler/FillerFactory.java
@@ -1,46 +1,129 @@
 package rs.edu.viser.services.filler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
+import rs.edu.viser.logger.LOG;
 import rs.edu.viser.services.filler.config.FillerConfigReader;
 import rs.edu.viser.services.filler.config.FillerGroupConfig;
 import rs.edu.viser.services.filler.groups.AccountFillerGroup;
 import rs.edu.viser.services.filler.groups.AchievementFillerGroup;
+import rs.edu.viser.services.filler.groups.CharacterFillerGroup;
+import rs.edu.viser.services.filler.groups.CommerceFillerGroup;
+import rs.edu.viser.services.filler.groups.DecorativeFillerGroup;
 import rs.edu.viser.services.filler.groups.FillerGroup;
+import rs.edu.viser.services.filler.groups.ItemFillerGroup;
+import rs.edu.viser.services.filler.groups.PvpFillerGroup;
+import rs.edu.viser.services.filler.groups.SystemFillerGroup;
+import rs.edu.viser.services.filler.groups.WorldFillerGroup;
+import rs.edu.viser.services.filler.groups.WvwFillerGroup;
 import rs.edu.viser.services.filler.groups.FillerGroup.FillerGroupTypes;
+import rs.edu.viser.services.scheduler.SchedulerTypes;
 
+/**
+ * Reads through the configuration reader and sets the values of 
+ * website and filler groups array.
+ * @author neman
+ *
+ */
 public class FillerFactory {
+	private FillerConfigReader reader;
+	private LOG log;
+	
 	private String website;
 	private FillerGroupConfig[] fillerGroups;
 	
+	/**
+	 * Reads through the config file and returns all filler groups.
+	 */
 	public FillerFactory() {
-		FillerConfigReader reader = FillerConfigReader.getFillerConfigReader();
+		this.reader = FillerConfigReader.getFillerConfigReader();
+		this.log = new LOG(this.getClass());
+		
+		log.info("Initializing filler factory. . . . . . . . .");
 		
 		this.website = reader.getWebsite();
 		this.fillerGroups = reader.getFillerGroups();
 	}
 	
-	public FillerFactory(FillerGroupTypes type) {
-		FillerConfigReader reader = FillerConfigReader.getFillerConfigReader();
-		
-		this.website = reader.getWebsite();
-		this.fillerGroups = reader.getFillerGroups(type);
+	/**
+	 * Replaces the type to just the types that have a group type specified.
+	 * @param type filler group type
+	 * @return this
+	 */
+	public FillerFactory fillerGroups(FillerGroupTypes type) {
+		//TODO: fix, currently excludes instead of the opposite
+		log.info("Filtering group types (" + type.toString() + ") ...");
+		List<FillerGroupConfig> newList = new LinkedList<>(Arrays.asList(this.fillerGroups));
+		newList.removeAll(Arrays.asList(reader.getFillerGroups(type)));
+		this.fillerGroups = newList.toArray(new FillerGroupConfig[newList.size()]);
+		return this;
 	}
 	
+	/**
+	 * Replaces the type to just the types that have a scheduler specified.
+	 * @param type type of scheduler
+	 * @return this
+	 */
+	public FillerFactory scheduler(SchedulerTypes type) {
+		//TODO: fix, currently excludes instead of the opposite
+		log.info("Filtering scheduler types (" + type.toString() + ") ...");
+		List<FillerGroupConfig> newList = new LinkedList<>(Arrays.asList(this.fillerGroups));
+		newList.removeAll(Arrays.asList(reader.getFillerGroups(type)));
+		this.fillerGroups = newList.toArray(new FillerGroupConfig[newList.size()]);
+		return this;
+	}
+	
+	/**
+	 * Returns a list of filler group classes.
+	 * @return
+	 */
 	public List<FillerGroup> getFillerGroup() {
 		List<FillerGroup> fillerGroupList = new ArrayList<>();
+		log.info("Importing filler groups...");
 		
+		//Check the type of the filler group and add the right filler class to the list.
 		for (FillerGroupConfig groupConf : fillerGroups) {
 			String fullWebsite = website + groupConf.getUrlSufix();
 			groupConf.setUrlSufix(fullWebsite);
 			
+			log.info("Searching for filler group for " + groupConf.getFillerGroupType());
+			
 			if (groupConf.getFillerGroupType() == FillerGroupTypes.ACCOUNT) {
 				fillerGroupList.add(new AccountFillerGroup());
+				
 			} else if (groupConf.getFillerGroupType() == FillerGroupTypes.ACHIEVEMENT) {
 				fillerGroupList.add(new AchievementFillerGroup());
+			
+			} else if (groupConf.getFillerGroupType() == FillerGroupTypes.CHARACTER) {
+				fillerGroupList.add(new CharacterFillerGroup());
+			
+			} else if (groupConf.getFillerGroupType() == FillerGroupTypes.COMMERCE) {
+				fillerGroupList.add(new CommerceFillerGroup());
+				
+			} else if (groupConf.getFillerGroupType() == FillerGroupTypes.DECORATIVE) {
+				fillerGroupList.add(new DecorativeFillerGroup());
+				
+			} else if (groupConf.getFillerGroupType() == FillerGroupTypes.ITEM) {
+				fillerGroupList.add(new ItemFillerGroup());
+				
+			} else if (groupConf.getFillerGroupType() == FillerGroupTypes.PVP) {
+				fillerGroupList.add(new PvpFillerGroup());
+				
+			} else if (groupConf.getFillerGroupType() == FillerGroupTypes.SYSTEM) { 
+				fillerGroupList.add(new SystemFillerGroup());
+				
+			} else if (groupConf.getFillerGroupType() == FillerGroupTypes.WORLD) {
+				fillerGroupList.add(new WorldFillerGroup());
+				
+			} else if (groupConf.getFillerGroupType() == FillerGroupTypes.WVW) {
+				fillerGroupList.add(new WvwFillerGroup());
+				
+			} else {
+				log.warning("Filler group not found! (" + groupConf.getFillerGroupType().toString() + ")");
 			}
-			//TODO: . . .
 		}
 		
 		return fillerGroupList;
