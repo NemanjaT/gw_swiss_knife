7bde9ce3e66aace4f22f9e69284064397d164fef
Nemanja Tozic <Nemanja.Tozic@crealogix.com>
2016-07-06 11:58:20 +0200

    Changed depending classes to FillerGroup, so stuff works.

--- gw_service/src/main/java/rs/edu/viser/services/filler/groups/WvwFillerGroup.java
+++ gw_service/src/main/java/rs/edu/viser/services/filler/groups/WvwFillerGroup.java
@@ -1,11 +1,26 @@
 package rs.edu.viser.services.filler.groups;
 
+import rs.edu.viser.services.filler.config.FillerArrayPatternConfig;
+import rs.edu.viser.services.filler.config.FillerObjectPatternConfig;
+
 public class WvwFillerGroup extends FillerGroup {
 
 	@Override
 	public void getModels(SchedulerTypes type) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	protected Object filterObjects(FillerObjectPatternConfig pattern) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object filterArrays(FillerArrayPatternConfig pattern) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
